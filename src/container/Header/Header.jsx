bf28e45438cab75c64c64b0a162aab207a90c58d
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-30 01:55:28 +0100

    header done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from 'react'
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
 
-import './Header.css';
+import './Header.css'
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title={'Chase The New Flavour'} />
+      <h1>The Key to Fine Dinning</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam
+        scelerisque sapien. Et, penatibus aliquam amet tellus
+      </p>
+      <button type='button' className='custom__button'>
+        Explore Menu
+      </button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='image_welcome' />
+    </div>
   </div>
-);
+)
 
-export default Header;
+export default Header
